a45695c62fcc74c8f0f2efb765b26e226b377e3b
justinait <justiiturraspe@gmail.com>
2023-05-19 14:37:16 -0300

    clean code comments

--- js/instagram.js
+++ js/instagram.js
@@ -12,18 +12,3 @@ document.addEventListener("DOMContentLoaded", function() {
 
     
 })
-// console.log(feedItem);
-
-// for(let i=0; i<feedItem.length; i++){
-//     console.log('feedItem[i]');
-    // feedItem[8].style.display = "none";
-
-    // navItem[i].addEventListener('click', () => {
-    //     for(let i=0; i<navItem.length; i++){
-    //         navItem[i].style.display = "block";
-    //         navItemActive[i].style.display = "none";
-    //     }
-    //     navItem[i].style.display = "none";
-    //     navItemActive[i].style.display = "block";
-    // })
-// 
