0ed0b923373083e574da2ceffb341b33a02d5db2
justinait <justiiturraspe@gmail.com>
2023-05-20 13:27:10 -0300

    yellow border / none

--- js/services.js
+++ js/services.js
@@ -7,15 +7,21 @@ for(let i = 0; i < servicePh.length; i++){
     servicePh[i].addEventListener('click', () => {
 
         for(let i = 0; i < servicePh.length; i++) {
-            servicePh[i].style.display = "block"
-            serviceDesc[i].style.display = "none"
+            servicePh[i].style.display = "block";
+            serviceDesc[i].style.display = "none";
+
+            service[i].style.borderTop = "yellow 1.7px solid";
         }
 
-        servicePh[i].style.display = "none"
-        serviceDesc[i].style.display = "block"
+        servicePh[i].style.display = "none";
+        serviceDesc[i].style.display = "block";
+
+        service[i].style.borderTop = "none";
 
     })
 }
 
 // border-top: yellow 1.7px solid;
 // border-top: none;
+
+//yo quiero q lo q sucede aca, suceda mas lento y agregando esas 2 props
