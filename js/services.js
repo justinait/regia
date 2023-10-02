8b5dffc94b5880844e77d4bfdc4a3acda1d0046f
justinait <justiiturraspe@gmail.com>
2023-10-02 10:36:19 -0300

    services css

--- js/services.js
+++ js/services.js
@@ -1,27 +1,43 @@
 const servicePh = document.getElementsByClassName('servicePhoto');
 const serviceDesc = document.getElementsByClassName('serviceDescription');
 const service = document.getElementsByClassName('service');
+if (window.innerWidth <= 1000) {
 
-for(let i = 0; i < servicePh.length; i++){
+    for(let i = 0; i < servicePh.length; i++){
 
-    servicePh[i].addEventListener('click', () => {
+        servicePh[i].addEventListener('click', () => {
+    
+            for(let i = 0; i < servicePh.length; i++) {
+                servicePh[i].style.display = "block";
+                serviceDesc[i].style.display = "none";
+    
+                service[i].style.borderTop = "yellow 1.7px solid";
+            }
+    
+            servicePh[i].style.display = "none";
+            serviceDesc[i].style.display = "block";
+    
+            service[i].style.borderTop = "none";
+    
+        })
+    }
+} 
 
-        for(let i = 0; i < servicePh.length; i++) {
-            servicePh[i].style.display = "block";
-            serviceDesc[i].style.display = "none";
-
-            service[i].style.borderTop = "yellow 1.7px solid";
-        }
-
-        servicePh[i].style.display = "none";
-        serviceDesc[i].style.display = "block";
-
-        service[i].style.borderTop = "none";
-
-    })
+if (window.innerWidth > 1000) {
+    
+    for (let i = 0; i < servicePh.length; i++) {
+        
+        servicePh[i].addEventListener('mouseover', () => {
+            
+            for (let j = 0; j < servicePh.length; j++) {
+                servicePh[j].style.display = "block";
+                serviceDesc[j].style.display = "none";
+                service[j].style.borderTop = "yellow 1.7px solid";
+            }
+            
+            servicePh[i].style.display = "none";
+            serviceDesc[i].style.display = "block";
+            service[i].style.borderTop = "none";
+        });
+    }
 }
-
-// border-top: yellow 1.7px solid;
-// border-top: none;
-
-//yo quiero q lo q sucede aca, suceda mas lento y agregando esas 2 props
